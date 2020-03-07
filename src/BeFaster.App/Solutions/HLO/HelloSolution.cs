153a810194ff64138fcd77e022811fb16558a175
sbx_user1051 <sbx_user1051@169.254.21.85>
2020-03-07 10:10:31 +0000

    Sat Mar 07 10:10:31 UTC 2020

--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -4,11 +4,9 @@ namespace BeFaster.App.Solutions.HLO
 {
     public static class HelloSolution
     {
-        private const string HELLO_WORLD = "Hello, World!";
         public static string Hello(string friendName)
         {
-            return HELLO_WORLD;
+            return $"Hello, {friendName}";
         }
     }
 }
-
