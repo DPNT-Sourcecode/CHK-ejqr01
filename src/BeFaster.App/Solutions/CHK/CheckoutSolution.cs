aa4bc5c49eea02ff350f7a3559a2e045d66faa56
sbx_user1051 <sbx_user1051@169.254.21.85>
2020-03-07 10:33:59 +0000

    Sat Mar 07 10:33:59 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,12 +1,29 @@
 ﻿using BeFaster.Runner.Exceptions;
+using System.Collections.Generic;
 
 namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
+        private static readonly IDictionary<string, int> priceList = new Dictionary<string, int>()
+        {
+            {"A",50 },
+            {"B",30 },
+            {"C",20 },
+            {"D",15 }
+        };
+        private static readonly IDictionary<string, int> offers = new Dictionary<string, int>()
+        {
+            {"3A", 130},
+            {"2B",45 }
+        };
         public static int ComputePrice(string skus)
         {
             throw new SolutionNotImplementedException();
         }
     }
 }
+A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15 
