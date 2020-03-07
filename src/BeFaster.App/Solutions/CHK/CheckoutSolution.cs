fa86ce203f6a1648287505892327aa7514005b23
sbx_user1051 <sbx_user1051@169.254.21.85>
2020-03-07 12:16:48 +0000

    Sat Mar 07 12:16:48 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -12,12 +12,14 @@ namespace BeFaster.App.Solutions.CHK
             {'A',50 },
             {'B',30 },
             {'C',20 },
-            {'D',15 }
+            {'D',15 },
+            {'E',40 }
         };
         private static readonly IDictionary<char, string> offers = new Dictionary<char, string>()
         {
-            {'A', "3-130"},
-            {'B',"2-45" }
+            {'A', "3-130 ,5-200"},
+            {'B',"2-45" },
+            {'E', "2-1*B" },
         };
         private static bool IsValidItem(char item)
         {
@@ -47,6 +49,8 @@ namespace BeFaster.App.Solutions.CHK
 
             var IndividualItems = shoppingList.Select(x => x).Distinct().ToList();
             var totalPrice = 0;
+            var discount = 0;
+
             IndividualItems.ForEach(item =>
             {
                 if (IsValidItem(item))
@@ -54,20 +58,37 @@ namespace BeFaster.App.Solutions.CHK
                     var noOfItems = shoppingList.Where(x => x == item).Count();
                     if (offers.ContainsKey(item))
                     {
-                        var offer = offers[item];
-                        var noInOffer = Int32.Parse(offer.Split('-')[0]);
-                        var withoutOffer = noOfItems % noInOffer;
-                        var withOffer = noOfItems / noInOffer;
-                        var offerdPrice = Int32.Parse(offer.Split('-')[1]);
-                        totalPrice = totalPrice + withOffer * offerdPrice + withoutOffer * priceList[item];
+                        var offer = offers[item].Split(',');
+                        var noInOffer = 0;
+                        var withoutOffer = 0;
+                        var withOffer = 0;
+                        var offerdPrice = 0;
+                        var itemTotalPrice = Int32.MaxValue;
+                        foreach (var o in offer)
+                        {
+                            noInOffer = Int32.Parse(o.Split('-')[0]);
+                            withoutOffer = noOfItems % noInOffer;
+                            withOffer = noOfItems / noInOffer;
+                            if(o.Split('*').Count()< 2) //Not offering other sku and only offers in price
+                                offerdPrice = Int32.Parse(o.Split('-')[1]);
+                            else
+                            {
+                                discount += withOffer * priceList[o.Split('*')[1].ToCharArray()[0]];
+                            }
+                            var total = withOffer * offerdPrice + withoutOffer * priceList[item];
+                            total = total <= itemTotalPrice ? totalPrice : itemTotalPrice;
+                        }
+
+                        totalPrice += itemTotalPrice;
                     }
                     else
                     {
-                        totalPrice = totalPrice + noOfItems * priceList[item];
+                        totalPrice += noOfItems * priceList[item];
                     }
                 }
             });
-            return totalPrice;
+            return totalPrice-discount;
         }
     }
 }
+
